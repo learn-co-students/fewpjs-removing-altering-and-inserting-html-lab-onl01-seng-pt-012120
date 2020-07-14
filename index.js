9d9a06a114b5d865306f83282f2dd15918e970db
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-14 09:46:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'Amgad is the champion';
+document.body.appendChild(element)
+
+element.appendChild(newHeader);
