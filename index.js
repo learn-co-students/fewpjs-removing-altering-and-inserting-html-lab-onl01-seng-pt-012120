f3b56360eefaf90990afd6f753990308c969ecdf
danielsun <danielsun12345@gmail.com>
2020-07-21 20:11:17 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+main.remove()
+let newHeader = document.createElement('H1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1>victory</h1><h1>Your-NAME is the champion</h1>";
