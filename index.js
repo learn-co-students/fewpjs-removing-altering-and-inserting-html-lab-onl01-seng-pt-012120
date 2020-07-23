5373b4b5a0f980a9a25f86561636ac1cc969eba3
Rick Belson <belsonrj@aol.com>
2020-07-23 00:37:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
