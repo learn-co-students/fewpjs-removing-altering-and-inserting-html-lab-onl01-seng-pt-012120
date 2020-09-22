0aade46aaaa1c6b73f9eb24fb65c63353efdaaab
N-Santiago <norbertosantiago82@gmail.com>
2020-09-22 15:01:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 main.remove()
 
-let newHeader = document.createElement('H1');
+const newHeader = document.createElement('H1');
 newHeader.id = 'victory'
 newHeader.innerHTML = "<h1>victory</h1><h1>YOUR-NAME is the champion</h1>";
 
